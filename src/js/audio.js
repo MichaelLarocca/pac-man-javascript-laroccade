69302ef224f99bd922e8f8cd482fb6550cd5ff4a
MichaelLarocca <michaellarocca@live.com>
2025-08-23 15:05:34 -0400

    Fix audio paths and preload sounds

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,15 +1,15 @@
 // Game sounds
-export const soundCutscene = new Audio("../assets/audio/cutscene.mp3");
-export const soundDeath = new Audio("../assets/audio/death.mp3");
-export const soundPacManEatingFruit = new Audio("../assets/audio/eating-fruit.mp3");
-export const soundEatingGhost = new Audio("../assets/audio/eating-ghost.mp3");
-export const soundGameStart = new Audio("../assets/audio/game-start.mp3");
-export const soundGhostRunningAway = new Audio("../assets/audio/ghost-running-away.mp3");
-export const soundGhostSiren1 = new Audio("../assets/audio/ghost-siren-1.mp3");
-export const soundGhostSiren2 = new Audio("../assets/audio/ghost-siren-2.mp3");
-export const soundHighScore = new Audio("../assets/audio/high-score.mp3");
-export const soundPacManEatingPellets = new Audio("../assets/audio/pac-man-eating-pellets.mp3");
-export const soundPowerUp = new Audio("../assets/audio/power-up.mp3");
+export const soundCutscene = new Audio("/assets/audio/cutscene.mp3");
+export const soundDeath = new Audio("/assets/audio/death.mp3");
+export const soundPacManEatingFruit = new Audio("/assets/audio/eating-fruit.mp3");
+export const soundEatingGhost = new Audio("/assets/audio/eating-ghost.mp3");
+export const soundGameStart = new Audio("/assets/audio/game-start.mp3");
+export const soundGhostRunningAway = new Audio("/assets/audio/ghost-running-away.mp3");
+export const soundGhostSiren1 = new Audio("/assets/audio/ghost-siren-1.mp3");
+export const soundGhostSiren2 = new Audio("/assets/audio/ghost-siren-2.mp3");
+export const soundHighScore = new Audio("/assets/audio/high-score.mp3");
+export const soundPacManEatingPellets = new Audio("/assets/audio/pac-man-eating-pellets.mp3");
+export const soundPowerUp = new Audio("/assets/audio/power-up.mp3");
 
 // Adjust sound volumes
 soundGhostSiren1.volume = 0.4;
